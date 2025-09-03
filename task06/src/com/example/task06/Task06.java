1c603539151bbb5ccea964d7e5820017c4f9bd94
Krivchenko Vyacheslav <krivchenko.vi74@gmail.com>
2025-09-03 14:29:35 +0500

    task06 completed

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,10 +3,15 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int sum = x + y;
+        int digitsAmount = 0;
+        if (sum == 0) return 1;
+        while (sum != 0) {
+            digitsAmount++;
+            sum /= 10;
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return digitsAmount;
     }
 
     public static void main(String[] args) {
